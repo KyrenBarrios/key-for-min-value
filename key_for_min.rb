2591e63f6cd50cf6127ca9a8c60ee520dc861415
Kyren Barrios <kyrenbarrios@gmail.com>
2020-11-13 23:20:47 -0800

    commit tree 182924916e7dbd7899b809ab4633f97a98da0446

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-    values_array = name_hash.collect{|key, value| value}.sort
-    name_hash.each{|key, value| return key if value == values_array[0]}
+def key_for_min_value(hash)
+    values_array = hash.collect{|key, value| value}.sort
+    hash.each{|key, value| return key if value == values_array[0]}
     nil
   end 
